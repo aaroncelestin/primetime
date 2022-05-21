b0f5aeb630cdbb0c054ab91dd8cc82132a42e2a4
aaroncelestin <51392307+aaroncelestin@users.noreply.github.com>
2022-05-21 15:13:32 -0500

    Add files via upload

---
+++ admin-home.php
@@ -0,0 +1,27 @@
+<?php
+namespace Primetime;
+require __DIR__ . '/src/functions.php';
+
+
+view('content-header');
+
+
+view('primetime-logo');
+?>
+
+<div class ="container-fluid"
+Search Patients
+ >require forms
+ >add Patient
+ >delete Patients
+ >modify patient data
+
+
+
+
+ Search users
+  >delete user
+  >reset user password
+  >change user info
+  >assign patient(s) to user
+  >delete patient(s) from user
