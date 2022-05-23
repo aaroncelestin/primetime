8b6acaf5f263571cf9002c55db1e0c043ad49285
aaroncelestin <51392307+aaroncelestin@users.noreply.github.com>
2022-05-23 13:38:33 -0500

    Update send-reset.php
    Reworked password reset request to do more checks on inputs before sending emails

--- src/send-reset.php
+++ src/send-reset.php
@@ -6,18 +6,25 @@ view('primetime-logo');
 
 session_start();
 if(isset($_REQUEST['email-reset-btn'])){
-    if(is_email($_REQUEST['email'])){
-        $user = find_user_by_email($_REQUEST['email']);
-        setup_reset($user);
-    }
-    else if(!empty(find_user_by_username($_REQUEST['username']))){
-        $user = find_user_by_username($_REQUEST['username']); 
-        setup_reset($user);
-    }
-    else {
-        echo ("Email or username not found. Please register or try again.");
+
+
+    if(is_email($_REQUEST['email']) || ($_REQUEST['username']) !== ""){
+
+        if (is_user($_REQUEST['username'], $_REQUEST['email'])){
+            $user = find_user_by_email($_REQUEST['email']) ?? find_user_by_username($_REQUEST['username']);
+            setup_reset($user);
+        }
+        else
+        {
+            echo ("Email or username not found. Please register or try again.");
+            header("refresh:5;url=send-reset.php");
+        }
+    }       
+    else if(empty($_REQUEST['email']) || empty($_REQUEST['username'])){
+        echo ("Please enter an email or username");
         header("refresh:5;url=send-reset.php");
     }
+   
 }
 
 ?>
