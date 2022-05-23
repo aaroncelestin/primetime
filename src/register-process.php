e5ff4e2d4df732fb0d4fb09914a0e20578a0cd14
aaroncelestin <51392307+aaroncelestin@users.noreply.github.com>
2022-05-22 22:47:38 -0500

    Update register-process.php
    Rewrote register routine, error reporting and input validation and sanitization. See functions.php for new methods.

--- src/register-process.php
+++ src/register-process.php
@@ -1,10 +1,7 @@
-
 <?php
 namespace Primetime;
 //REGISTRATION LOGIC
 session_start();
-$errors = [];
-$inputs = [];
 require __DIR__ . '/functions.php';
 //if user session is already set and logged in, redirect to homepage
 if(isset($_SESSION['username'])){
@@ -15,30 +12,30 @@ if(isset($_SESSION['username'])){
 if(isset($_REQUEST['register-btn'])){
     $username = filter_var($_REQUEST['username'],FILTER_SANITIZE_SPECIAL_CHARS);
     $email = filter_var($_REQUEST['email'],FILTER_SANITIZE_EMAIL);
-    $password = $_REQUEST['password'];
+    $password = $_REQUEST['password'];//dont clean passwords
     $confirmpassword = $_REQUEST['confirmpassword'];
-    //check if email is already registered
-    $email_stmt = $db->prepare("SELECT username,email FROM ptusers WHERE email =:email");
-    $email_stmt->execute([':email' => $email]);
-    if(isset($row['email']) == $email){
-		//throw error if that email exists and redirect to login
-        flash('email', );
-	    redirect_to('/../login.php');
-          }                
-
-    // custom messages  
-    else if (!$errors) {
-        //register user
-        register_user($inputs['email'], $inputs['username'], $inputs['password']);
+    if ((int)is_user($username, $email) === 0 && $password == $confirmpassword){
+        register_user($email, $username, $password);
         $activation_code = generate_activation_code(); //email activation code 
-        send_activation_email($inputs['email'], $activation_code);
-       
+        send_activation_email($email, $activation_code);
         redirect_to(
-            '/../config/activate.php',
-            'Please check your email and click the link to verify your email address.'
-            );
-        }
+        '/../config/activate.php',
+        'Please check your email and click the link to verify your email address.'
+        );
+    } 
+    else if ($password != $confirmpassword) {
+        flash('same', 'password');
+        flush();
+        header('refresh: 3');
     }
- else if (is_get_request()) {
-    [$inputs, $errors] = session_flash('inputs', 'errors');
+    else if ((int)is_user($username, $email) === 1){
+        flash('unique', 'email');
+        flush();
+        redirect_to('/../register.php');
+    }
+    else {
+        flash('generic', 'email');
+        flush();
+        header ('refresh: 3');
+    }
 }
