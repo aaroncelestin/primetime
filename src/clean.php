4897f25eb9213c6269d98686443960b387f6139c
aaroncelestin <51392307+aaroncelestin@users.noreply.github.com>
2022-05-22 22:52:45 -0500

    Update clean.php
    Added overloaded input fields for flash and format_flash with optional scalar args
    Added generic error message

--- src/clean.php
+++ src/clean.php
@@ -2,6 +2,7 @@
 namespace Primetime;
 const REQUIRED_ERROR = "This field is required.";
 const FORMATTED_ERRORS = [
+    'login' => 'Invalid username or password',
     'required' => 'The %s is required',
     'email' => 'This is not a valid email address',
     'min' => 'The %s must have at least %d characters',
@@ -11,7 +12,8 @@ const FORMATTED_ERRORS = [
     'alphanumeric' => 'The %s should have only letters and numbers',
     'secure' => 'The %s must have between 8 and 64 characters and contain at least one number, one upper case letter, one lower case letter and one special character',
     'unique' => 'The %s already exists',
-    'numbers' => 'The %s can only contain numbers, no letters'
+    'numbers' => 'The %s can only contain numbers, no letters',
+    'generic' =>  'There was an error processing your request'
 ];
 
 
@@ -20,7 +22,8 @@ function clean_text(string $data):string {
     $data = stripslashes($data);
     $data = htmlspecialchars($data);
     return $data;
-  }
+}
+
 
 /**
  * Removes any English alphabet chars from an input string and returns an int
@@ -54,12 +57,16 @@ function clean_number(string $data):int {
     return $num;
 }
 
-function flash (string $particular_error, array $error_type): string  {
-    foreach ($error_type as $errors){
-        $msg = format_flash(FORMATTED_ERRORS, $error_type, $particular_error)
-        $error_div = "<div class="alert alert-warning alert-dismissible fade show"><?.$msg.?></div>";
-    }
-    return $msg;
+/**
+ * Show flash message of DEFAULT_ERRORS with given $particular_error index
+ * returns HTML string with div alert
+ */
+function flash (string $particular_error, string $field): string  {
+    
+        $msg = format_flash(FORMATTED_ERRORS, $particular_error, $field);
+        $error_div = '<div class="alert alert-warning alert-dismissible fade show"><?.$msg.?></div>';
+    
+    return $error_div;
 }
 
 /**
@@ -69,45 +76,11 @@ function flash (string $particular_error, array $error_type): string  {
  * @param array $errmsg_array global array of msgs
  * @param string $error_type index of array
  * @param string $field given by caller
- * @param int $scalar1 given by caller
  * @return string formatted
  */
-function format_flash (array $errmsg_array, string $error_type, string $field, int $scalar) {
+function format_flash (array $errmsg_array, string $error_type, string $field) {
     $temp_message = $errmsg_array[$error_type];
-    return sprintf($temp_message, $field, $scalar); 
+    return sprintf($temp_message, $field); 
 }
 
 
-/**
- * Format and fill flash message of given type with field and scalar using sprintf
- * For example: This $field must be $scalar long
- * 
- * @param array $errmsg_array global array of msgs
- * @param string $error_type index of array
- * @param string $field given by caller
- * @param int $scalar1 given by caller
- * @param int $scalar2 given by caller
- * @return string formatted
- */
-function format_flash (array $errmsg_array, string $error_type, string $field, int $scalar1, int $scalar2) {
-    $temp_message = $errmsg_array[$error_type];
-    return sprintf($temp_message, $field, $scalar1, $scalar2); 
-}
-
-
-/**
- * Format and fill flash message of given type with field and scalar using sprintf
- * For example: This $field must be $scalar long
- * 
- * @param array $errmsg_array global array of msgs
- * @param string $error_type index of array
- * @param string $field given by caller
- * @param int $scalar1 given by caller
- * @param int $scalar2 given by caller
- * @param int $scalar3 given by caller
- * @return string formatted
- */
-function format_flash (array $errmsg_array, string $error_type, string $field, int $scalar1, int $scalar2, int $scalar3) {
-    $temp_message = $errmsg_array[$error_type];
-    return sprintf($temp_message, $field, $scalar1, $scalar2, $scalar3); 
-}
