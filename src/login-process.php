80ffefc689777d8860aeab9380c647866304bd13
aaroncelestin <51392307+aaroncelestin@users.noreply.github.com>
2022-05-23 22:04:47 -0500

    Update login-process.php
    Rewrote login routine to make it faster and more secure

--- src/login-process.php
+++ src/login-process.php
@@ -1,38 +1,36 @@
 <?php
 namespace Primetime;
 require __DIR__ . '/functions.php';
+ob_start();
 if (isset($_SESSION['username'])) {
+    flush();
     redirect_to('/../home.php');
+    die;
 }
-
-$inputs = [];
-$errors = [];
 
 if ($_SERVER["REQUEST_METHOD"]== "POST") {
     if (empty($_POST["username"])) {
-        $user_error = "Name is required";
-      } else {
-        $username = clean_text($_POST["username"]);
-      }
-    // if login fails 
-    if (!login($inputs['username'], $inputs['password'])) {
-
-        $errors['login'] = 'Invalid username or password';
-
-        redirect_with('/../login.php', [
-            'errors' => $errors,
-            'inputs' => $inputs
-        ]);
-    }
-    else if (login($inputs['username'], $inputs['password'])) {
-    //check if user logging in is_admin
-        if ((int)$_SESSION['is_admin'] === 1){
-            redirect_to('%HOME_URL/admin_home.php');}
-        else {
+        $user_error = "username is required";
+    } 
+    else {
+    $username = clean_text($_POST["username"]);
+    $password = $_POST['password'];
+    if (login($username,$password)){
+        if($_SESSION['is_admin']==0){
+            flush();
             redirect_to('/../home.php');
+            die;
         }
+        else if ($_SESSION['is_admin']==1){
+            flush();
+            redirect_to('/../admin_home.php');
+            die;
+        } 
     }
-    
-} else if (is_get_request()) {
-    [$errors, $inputs] = session_flash('errors', 'inputs');
+    else {
+        flash('login', 'login');
+        flush();
+        header('refresh: 5');
+    }
+    }
 }
