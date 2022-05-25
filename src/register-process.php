001474ef30a178678986ee5f5265a3e44e082b5d
aaroncelestin <51392307+aaroncelestin@users.noreply.github.com>
2022-05-25 16:49:47 -0500

    Update register-process.php
    Added client side JS code to validate user input that then passes off to PHP function for server side sanitization

--- src/register-process.php
+++ src/register-process.php
@@ -7,35 +7,204 @@ require __DIR__ . '/functions.php';
 if(isset($_SESSION['username'])){
     header("location: /../home.php");
 }
+?>
 
 
-if(isset($_REQUEST['register-btn'])){
-    $username = filter_var($_REQUEST['username'],FILTER_SANITIZE_SPECIAL_CHARS);
-    $email = filter_var($_REQUEST['email'],FILTER_SANITIZE_EMAIL);
-    $password = $_REQUEST['password'];//dont clean passwords
-    $confirmpassword = $_REQUEST['confirmpassword'];
-    if ((int)is_user($username, $email) === 0 && $password == $confirmpassword){
-        register_user($email, $username, $password);
-        $activation_code = generate_activation_code(); //email activation code 
-        send_activation_email($email, $activation_code);
-        redirect_to(
-        '/../config/activate.php',
-        'Please check your email and click the link to verify your email address.'
-        );
-    } 
-    else if ($password != $confirmpassword) {
-        flash('same', 'password');
-        flush();
-        header('refresh: 3');
+<script>
+const usernameEl = document.querySelector('#username');
+const emailEl = document.querySelector('#email');
+const passwordEl = document.querySelector('#password');
+const confirmPasswordEl = document.querySelector('#confirm-password');
+const form = document.querySelector('#register');
+
+
+const checkUsername = () => {
+    let valid = false;
+    const min = 3,
+        max = 25;
+    const username = usernameEl.value.trim();
+    if (!isRequired(username)) {
+        showError(usernameEl, 'Username cannot be blank.');
+    } else if (!isBetween(username.length, min, max)) {
+        showError(usernameEl, `Username must be between ${min} and ${max} characters.`)
+    } else {
+        showSuccess(usernameEl);
+        valid = true;
     }
-    else if ((int)is_user($username, $email) === 1){
-        flash('unique', 'email');
-        flush();
-        redirect_to('/../register.php');
+    return valid;
+};
+
+
+const checkEmail = () => {
+    let valid = false;
+    const email = emailEl.value.trim();
+    if (!isRequired(email)) {
+        showError(emailEl, 'Email cannot be blank.');
+    } else if (!isEmailValid(email)) {
+        showError(emailEl, 'Email is not valid.')
+    } else {
+        showSuccess(emailEl);
+        valid = true;
     }
-    else {
-        flash('generic', 'email');
-        flush();
-        header ('refresh: 3');
+    return valid;
+};
+
+const checkPassword = () => {
+    let valid = false;
+    const password = passwordEl.value.trim();
+    if (!isRequired(password)) {
+        showError(passwordEl, 'Password cannot be blank.');
+    } else if (!isPasswordSecure(password)) {
+        showError(passwordEl, 'Password must has at least 8 characters that include at least 1 lowercase character, 1 uppercase characters, 1 number, and 1 special character in (!@#$%^&*)');
+    } else {
+        showSuccess(passwordEl);
+        valid = true;
     }
+    return valid;
+};
+
+const checkConfirmPassword = () => {
+    let valid = false;
+    // check confirm password
+    const confirmPassword = confirmPasswordEl.value.trim();
+    const password = passwordEl.value.trim();
+    if (!isRequired(confirmPassword)) {
+        showError(confirmPasswordEl, 'Please enter the password again');
+    } else if (password !== confirmPassword) {
+        showError(confirmPasswordEl, 'The password does not match');
+    } else {
+        showSuccess(confirmPasswordEl);
+        valid = true;
+    }
+
+    return valid;
+};
+
+const isEmailValid = (email) => {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+};
+
+const isPasswordSecure = (password) => {
+    const re = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+    return re.test(password);
+};
+
+const isRequired = value => value === '' ? false : true;
+const isBetween = (length, min, max) => length < min || length > max ? false : true;
+
+
+const showError = (input, message) => {
+    // get the form-field element
+    const formField = input.parentElement;
+    // add the error class
+    formField.classList.remove('success');
+    formField.classList.add('error');
+
+    // show the error message
+    const error = formField.querySelector('small');
+    error.textContent = message;
+};
+
+const showSuccess = (input) => {
+    // get the form-field element
+    const formField = input.parentElement;
+
+    // remove the error class
+    formField.classList.remove('error');
+    formField.classList.add('success');
+
+    // hide the error message
+    const error = formField.querySelector('small');
+    error.textContent = '';
 }
+
+
+form.addEventListener('submit', function (e) {
+    // prevent the form from submitting
+    e.preventDefault();
+
+    // validate fields
+    let isUsernameValid = checkUsername(),
+        isEmailValid = checkEmail(),
+        isPasswordValid = checkPassword(),
+        isConfirmPasswordValid = checkConfirmPassword();
+
+    let isFormValid = isUsernameValid &&
+        isEmailValid &&
+        isPasswordValid &&
+        isConfirmPasswordValid;
+
+    // submit to the server if the form is valid
+    if (isFormValid) {
+        <? "process_register();" ?>
+    }
+});
+
+
+const debounce = (fn, delay = 500) => {
+    let timeoutId;
+    return (...args) => {
+        // cancel the previous timer
+        if (timeoutId) {
+            clearTimeout(timeoutId);
+        }
+        // setup a new timer
+        timeoutId = setTimeout(() => {
+            fn.apply(null, args)
+        }, delay);
+    };
+};
+
+form.addEventListener('input', debounce(function (e) {
+    switch (e.target.id) {
+        case 'username':
+            checkUsername();
+            break;
+        case 'email':
+            checkEmail();
+            break;
+        case 'password':
+            checkPassword();
+            break;
+        case 'confirm-password':
+            checkConfirmPassword();
+            break;
+    }
+}));
+</script>
+<?php
+//PHP server side processing of registration
+function process_register (){
+    if(isset($_REQUEST['register'])){
+        $username = filter_var($_REQUEST['username'],FILTER_SANITIZE_SPECIAL_CHARS);
+        $email = filter_var($_REQUEST['email'],FILTER_SANITIZE_EMAIL);
+        $password = $_REQUEST['password'];//dont clean passwords
+        $confirmpassword = $_REQUEST['confirm-password'];
+        if ((int)is_user($username, $email) === 0 && $password == $confirmpassword){
+            register_user($email, $username, $password);
+            $activation_code = generate_activation_code(); //email activation code 
+            send_activation_email($email, $activation_code);
+            redirect_to(
+            '/../config/activate.php',
+            'Please check your email and click the link to verify your email address.'
+            );
+        } 
+        else if ($password != $confirmpassword) {
+            flash('same', 'password');
+            flush();
+            header('refresh: 3');
+        }
+        else if ((int)is_user($username, $email) === 1){
+            flash('unique', 'email');
+            flush();
+            redirect_to('/../register.php');
+        }
+        else {
+            flash('generic', 'email');
+            flush();
+            header ('refresh: 3');
+        }
+    }
+    }
+?>
